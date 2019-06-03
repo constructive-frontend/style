571fffbb6023947d6bdfafc261a6398c13b93bf8
Oleksiy Hryshchenko <greeschenko@gmail.com>
2019-06-03 14:44:47 +0000

    up

--- src/js/navs.js
+++ src/js/navs.js
@@ -1,10 +1,20 @@
-$(window).ready(function() {
-    $('.navs').each(function() {
-        $(this).find('a').bind('click', function() {
+class Navs {
+    constructor(el) {
+        this.el = el;
+        this.init();
+    }
+    init() {
+        this.el.find('a').bind('click', function() {
             if ($(this).next().prop('tagName') == 'UL') {
                 $(this).next().slideToggle();
                 return false;
             }
         });
+    }
+}
+
+$(window).ready(function() {
+    $('.navs').each(function() {
+        new Navs($(this));
     });
 });
